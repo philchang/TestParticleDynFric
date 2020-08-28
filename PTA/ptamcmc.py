2d34899cb2d3d0267e0bf23657de6afa26067e87
Philip Chang <chang65@uwm.edu>
2020-08-28 13:32:38 -0500

    updated with saveing file

--- PTA/ptamcmc.py
+++ PTA/ptamcmc.py
@@ -7,8 +7,12 @@ except :
 import numpy as np
 import scipy as sp
 import math
+import emcee
+import os
 
-iterations = 50000
+filename = "mcmc.h5" #default file, override with -o
+
+iterations = 20000
 thin = 200
 discard = 5000
 unique_name = True
@@ -17,8 +21,11 @@ excluded_pulsars = None
 nanograv = [0,1,2,3]
 ppta = [4,5,6,7,8,9]
 epta = [10,11,12,13]
+#best_pulsars = [0, 12, 2, 3, 4, 5, 6, 9, 10, 11]
 best_pulsars = [0, 12, 2, 3, 4, 5, 6, 9, 10, 11]
+
 use_other_distance = [5, 7, 9]
+#best_pulsars = nanograv
 pulsars_number = None
 pulsar_data = None
 
@@ -29,8 +36,9 @@ GAUSSIAN = 2
 number_parameters = 1 # number of parameters for the galactic model
 
 MODEL = QUILLEN
+
 if MODEL == QUILLEN : 
-    number_parameters = 2
+    number_parameters = 1
 elif MODEL == EXPONENTIAL or MODEL == GAUSSIAN :
     number_parameters = 2
 
@@ -131,7 +139,7 @@ def alos(x,y,z,parameter1,parameter2, Vlsr):
 
     return ((ax-axsun)*dx + (ay-aysun)*dy + (az-azsun)*dz)/dr
 
-def alos_from_pulsar(d, b, l, mus, lgalpha1, lgalpha2, Vlsr) : 
+def alos_from_pulsar(d, b, l, mus, alos_gr, lgalpha1, lgalpha2, Vlsr) : 
     ## pulsar positions --
     x = d * np.cos(b*np.pi/180.) * np.cos(l*np.pi/180.) *kpctocm + xsun
     y = d * np.cos(b*np.pi/180.) * np.sin(l*np.pi/180.) * kpctocm + ysun
@@ -142,7 +150,7 @@ def alos_from_pulsar(d, b, l, mus, lgalpha1, lgalpha2, Vlsr) :
     # include sloskey effect mu^2 *d /c
     al_sl = mus*mus*d*kpctocm/c
 
-    return al+al_sl
+    return al+al_sl + alos_gr
 
 def string_to_value_and_error( string, has_exponent=False) :
     import re
@@ -201,18 +209,28 @@ def unpack_theta( theta, number_pulsars) :
     mus = theta[number_parameters+number_pulsars:]
     return parameter1, parameter2, Vlsr, distances, mus
 
-def log_likelihood( theta) :
+def model_and_data( theta) : 
     global pulsar_data
     number_pulsars = pulsar_data["number pulsars"]
     lgalpha1, lgalpha2, Vlsr, distances, mus = unpack_theta(theta, number_pulsars)
     b = pulsar_data["latitude"]
     l = pulsar_data["longitude"]
-    alos_model = alos_from_pulsar( distances, b, l, mus, lgalpha1, lgalpha2, Vlsr)
+    alos_gr = pulsar_data["alos_gr"]
+    alos_model = alos_from_pulsar( distances, b, l, mus, alos_gr, lgalpha1, lgalpha2, Vlsr)
     alos_obs = pulsar_data["alos"]
     alos_err = pulsar_data["alos error"]
 
-    ll = 0.5*np.sum( -((alos_obs - alos_model)/alos_err)**2 - np.log(2*np.pi*alos_err*alos_err))
-    return ll
+    return alos_model, alos_obs, alos_err
+
+def log_likelihood( theta, return_chisq = False) :
+    alos_model, alos_obs, alos_err = model_and_data( theta)
+    if( not return_chisq) :
+        ll = 0.5*np.sum( -((alos_obs - alos_model)/alos_err)**2 - np.log(2*np.pi*alos_err*alos_err))
+        ll = 0.5*np.sum( -((alos_obs - alos_model)/alos_err)**2)# - np.log(2*np.pi*alos_err*alos_err))
+
+        return ll
+    else :
+        return np.sum(((alos_obs - alos_model)/alos_err)**2)
 
 def log_prior( theta) :
     global pulsar_data 
@@ -240,8 +258,8 @@ def log_prior( theta) :
             return -np.inf
 
     #lp += -0.5*(((Vlsr - Vlsr0)/Vlsr_err)**2 + math.log(2*math.pi*Vlsr_err**2))
-    lp += 0.5*np.sum(-((distances-distance_arr)/distance_err)**2 - np.log(2*np.pi*distance_err**2))
-    lp += 0.5*np.sum(- ((mus-mu_arr)/mu_err)**2 - np.log(2*np.pi*mu_err**2))
+    lp += 0.5*np.sum(-((distances-distance_arr)/distance_err)**2)# - np.log(2*np.pi*distance_err**2))
+    lp += 0.5*np.sum(- ((mus-mu_arr)/mu_err)**2)# - np.log(2*np.pi*mu_err**2))
     return lp
 
 def log_probability(theta) :
@@ -251,131 +269,203 @@ def log_probability(theta) :
         return -np.inf
     return lp + log_likelihood( theta)
 
+def read_pulsar_data() :
+    global pulsar_data 
+    import pandas as pd
 
-import pandas as pd
+    # import the GR corrections
+    gr_dataframe = pd.read_excel("PBDOT_GR.xls")
+    gr_names = gr_dataframe["Pulsar Name"]
+    gr_pbdot = gr_dataframe["PBDOT_GR (s/s)"]
 
-# import the data and decode
-dataframe = pd.read_excel("PBDOT.xls") 
-distances = dataframe['Parallax Distance (kpc)'] 
-names = dataframe["Pulsar Name"]
-datasets = dataframe["Dataset"]
-mus = dataframe["Proper Motion (mu, mas/yr)"]
-pbs = dataframe["PB (d)"]
-pbdot_obs = dataframe["PBDOT_obs (s/s)"]
-longitude = dataframe["Galactic Longitude (l, deg)"]
-latitude = dataframe["Galactic Latitude (b, deg)"]
-other_distances = dataframe["Other Distance (kpc)"]
+    # import the data and decode
+    dataframe = pd.read_excel("PBDOT.xls") 
+    distances = dataframe['Parallax Distance (kpc)'] 
+    names = dataframe["Pulsar Name"]
+    datasets = dataframe["Dataset"]
+    mus = dataframe["Proper Motion (mu, mas/yr)"]
+    pbs = dataframe["PB (d)"]
+    pbdot_obs = dataframe["PBDOT_obs (s/s)"]
+    longitude = dataframe["Galactic Longitude (l, deg)"]
+    latitude = dataframe["Galactic Latitude (b, deg)"]
+    other_distances = dataframe["Other Distance (kpc)"]
 
-name_arr = []
-dataset_arr = []
-distance_arr = []
-distance_err = []
-latitude_arr = []
-longitude_arr = []
-mu_arr = []
-mu_err = []
+    name_arr = []
+    dataset_arr = []
+    distance_arr = []
+    distance_err = []
+    latitude_arr = []
+    longitude_arr = []
+    mu_arr = []
+    mu_err = []
 
-alos_arr = []
-alos_err = []
+    alos_gr_arr = []
+    alos_gr_err = []
+    alos_arr = []
+    alos_err = []
 
-for name, dataset, distance_str, other_distance_str, mu_str, pb_str, pbdot_str, lat_str, long_str, pulsar_no in \
-        zip(names, datasets, distances, other_distances, mus, pbs, pbdot_obs, latitude, longitude, range(names.size)):
-    if( (unique_name and name in name_arr) or (not excluded_pulsars is None and name in excluded_pulsars)) : 
-        continue
-    if( not pulsars_number is None and not pulsar_no in pulsars_number) :
-        continue
-    if( not best_pulsars is None and not pulsar_no in best_pulsars) :
-        continue
-    name_arr.append(name) 
-    dataset_arr.append(dataset)
-    d, derr = string_to_value_and_error(distance_str)
-
-    if(pulsar_no in use_other_distance) : 
+    for name, dataset, distance_str, other_distance_str, mu_str, pb_str, pbdot_str, lat_str, long_str, pulsar_no in \
+            zip(names, datasets, distances, other_distances, mus, pbs, pbdot_obs, latitude, longitude, range(names.size)):
+        if( (unique_name and name in name_arr) or (not excluded_pulsars is None and name in excluded_pulsars)) : 
+            continue
+        if( not pulsars_number is None and not pulsar_no in pulsars_number) :
+            continue
+        if( not best_pulsars is None and not pulsar_no in best_pulsars) :
+            continue
+        name_arr.append(name) 
+        dataset_arr.append(dataset)
         d, derr = string_to_value_and_error(distance_str)
-    mu, muerr = string_to_value_and_error( mu_str)
-    mu *= mas_per_year_to_as_per_sec
-    muerr *= mas_per_year_to_as_per_sec
-    pbdot, pbdoterr = string_to_value_and_error( pbdot_str, has_exponent=True) 
-    pb, pberr = string_to_value_and_error( pb_str)
-    pb *= day_to_sec
 
-    distance_arr.append(d)
-    distance_err.append( derr)
-    mu_arr.append( mu)
-    mu_err.append( muerr)
-    alos_arr.append(pbdot/pb*c)
-    alos_err.append(pbdoterr/pb*c)
-    latitude_arr.append(float(lat_str))
-    longitude_arr.append(float(long_str))
-    print(name, dataset)
+        if(pulsar_no in use_other_distance) : 
+            d, derr = string_to_value_and_error(distance_str)
 
-longitude_arr = np.array(longitude_arr)
-latitude_arr = np.array(latitude_arr)
-distance_arr = np.array(distance_arr)
-distance_err = np.array(distance_err)
-mu_arr = np.array(mu_arr)
-mu_err = np.array(mu_err)
-alos_arr = np.array(alos_arr)
-alos_err = np.array(alos_err)
+        pbdot_gr = 0
+        pbdot_gr_err = 0
 
-pulsar_data = {"name" : name_arr, "dataset": dataset_arr, "longitude": longitude_arr, "latitude" : latitude_arr, \
-                "distance" : distance_arr, "distance error" : distance_err, "mu" : mu_arr, "mu error" : mu_err, \
-                "alos" : alos_arr, "alos error" : alos_err, "number pulsars" : len(name_arr)}
+        if(name in gr_names) : 
+            pbdot_gr_str = gr_pbdot[gr_names == name]
+            pbdot_gr, pbdot_gr_err = string_to_value_and_error( pbdot_gr_str, has_exponent=True)
+
+        mu, muerr = string_to_value_and_error( mu_str)
+        mu *= mas_per_year_to_as_per_sec
+        muerr *= mas_per_year_to_as_per_sec
+        pbdot, pbdoterr = string_to_value_and_error( pbdot_str, has_exponent=True) 
+        pb, pberr = string_to_value_and_error( pb_str)
+        pb *= day_to_sec
+
+        distance_arr.append(d)
+        distance_err.append( derr)
+        mu_arr.append( mu)
+        mu_err.append( muerr)
+        alos_arr.append(pbdot/pb*c)
+        alos_err.append(pbdoterr/pb*c)
+        latitude_arr.append(float(lat_str))
+        longitude_arr.append(float(long_str))
+        alos_gr_arr.append(float(pbdot_gr/pb*c))
+        alos_gr_err.append(float(pbdot_gr_err/pb*c))
+        print(name, dataset)
+
+    longitude_arr = np.array(longitude_arr)
+    latitude_arr = np.array(latitude_arr)
+    distance_arr = np.array(distance_arr)
+    distance_err = np.array(distance_err)
+    mu_arr = np.array(mu_arr)
+    mu_err = np.array(mu_err)
+    alos_arr = np.array(alos_arr)
+    alos_err = np.array(alos_err)
+    alos_gr_arr = np.array(alos_gr_arr)
+    alos_gr_err = np.array(alos_gr_err)
+
+    pulsar_data = {"name" : name_arr, "dataset": dataset_arr, "longitude": longitude_arr, "latitude" : latitude_arr, \
+                    "distance" : distance_arr, "distance error" : distance_err, "mu" : mu_arr, "mu error" : mu_err, \
+                    "alos_gr" : alos_gr_arr, "alos_gr_err" : alos_gr_err, \
+                    "alos" : alos_arr, "alos error" : alos_err, "number pulsars" : len(name_arr)}
+
+def run_mcmc() :
+
+    itheta = initialize_theta( frac_random=0.)
+
+    nwalkers = 100
+    ndim = itheta.size
+
+    pos = np.zeros( [nwalkers,ndim])
+
+    for i in range(nwalkers) : 
+        pos[i,:] = initialize_theta()
+
+    import multiprocessing
+
+    #with multiprocessing.Pool(1) as pool : 
+    #    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, pool=pool)
+    backend = None
+    if( not filename is None) : 
+        if os.path.exist(filename):
+            os.remove(filename)
+
+        backend = emcee.backends.HDFBackend(filename)
+        backend.reset(nwalkers, ndim)
+
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, backend=backend)
+    sampler.run_mcmc(pos, iterations, progress=True)
+    flat_samples = sampler.get_chain(discard=discard, thin=thin, flat=True)
+
+    return flat_samples 
+
+def read_samples():
+    reader = emcee.backends.HDFBackend(filename)
+    flat_samples = reader.get_chain(discard=discard, flat=True, thin=thin)
+
+    return flat_samples
+
+def make_corner_plot(flat_samples) :
+    global pulsar_data
+    import matplotlib.pyplot as pl
+    import corner
+
+    itheta = initialize_theta( frac_random=0.)
+
+    best_fit_theta = np.zeros(itheta.size)
+    for i in range(best_fit_theta.size) : 
+        mcmc = np.percentile(flat_samples[:, i], [50])
+        best_fit_theta[i] = mcmc[0]
+
+    pl.clf()
+    labels = [r"$\log(\alpha_1)$",r"$\log(-\alpha_2)$", r"$V_{\rm lsr}$"]
+    if MODEL == EXPONENTIAL or MODEL == GAUSSIAN : 
+        labels = [r"$\log(\rho_0/1\,M_{\odot}\,{\rm pc}^{-3})$",r"$\log(z_0/1\,{\rm pc})$", r"$V_{\rm lsr}$"]
+        flat_samples[:,0] -= math.log10(2e33/pc**3)
+        flat_samples[:,1] -= math.log10(pc)
+    fig = corner.corner( flat_samples[:,0:number_parameters], labels=labels[0:number_parameters],truths=best_fit_theta[0:number_parameters])
+    pl.savefig("corner.pdf")
+
+    for i in range(number_parameters):
+        mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
+        q = np.diff(mcmc)
+        txt = "\mathrm{{{3}}} = {0:.3f}_{{-{1:.3f}}}^{{{2:.3f}}}"
+        print( "{0} = {1} {2} {3}".format(labels[i], mcmc[1], q[0], q[1]))
 
 
-import emcee
+    print("best fit chisq = ", log_likelihood( best_fit_theta, return_chisq =True))
 
-itheta = initialize_theta( frac_random=0.)
+    alos_model, alos_obs, alos_err = model_and_data(best_fit_theta)
+    print(alos_model)
+    print(alos_obs)
+    print(alos_err)
+    pl.clf()
 
-nwalkers = 100
-ndim = itheta.size
+    pl.errorbar(range(alos_obs.size),alos_obs, yerr=alos_err, fmt=".",alpha=0.5,label=r"$a_{\rm los, obs}$")
+    pl.scatter(range(alos_model.size), alos_model, c='red', s=2, alpha=1,label=r"$a_{\rm los, mod}$")
 
-pos = np.zeros( [nwalkers,ndim])
+    # inds = np.random.randint(len(flat_samples), size=1000)
 
-for i in range(nwalkers) : 
-    pos[i,:] = initialize_theta()
+    # for ind in inds:
+    #     theta = flat_samples[ind]
+    #     alos_model, alos_obs, alos_err = model_and_data(best_fit_theta)
+    #     pl.scatter(range(alos_model.size), alos_model, c='red', s=2, alpha=0.5)
 
-import multiprocessing
+    names = np.array(pulsar_data["name"])
+    pl.ylim(3e-10,1e-6)
+    pl.xticks(range(alos_model.size), labels=names, rotation="vertical")
+    pl.yscale('log')
+    pl.legend(loc="best")
+    pl.savefig("test.pdf")
 
-#with multiprocessing.Pool(1) as pool : 
-#    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, pool=pool)
-sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability)
-sampler.run_mcmc(pos, iterations, progress=True)
+import argparse
+parser = argparse.ArgumentParser(description='Run calibration')
+parser.add_argument('-o', help="hdf5 file to store/load mcmc chain")
+parser.add_argument('--load_previous', action='store_true',
+                    default=False,
+                    help="load MCMC from file")
+args = parser.parse_args()
 
-samples = sampler.get_chain()
-import matplotlib.pyplot as pl
-# pl.clf()
-# fig, axes = pl.subplots(3, figsize=(10, 7), sharex=True)
-# labels = ["lgalpha1", "lgalpha2", "d"]
-# for i in range(3):
-#     ax = axes[i]
-#     ax.plot(samples[:, :, i], "k", alpha=0.3)
-#     ax.set_xlim(0, len(samples))
-#     ax.set_ylabel(labels[i])
-#     ax.yaxis.set_label_coords(-0.1, 0.5)
+if not args.o is None :
+    filename = args.o
 
-# pl.savefig("test.pdf")
+read_pulsar_data()
+flat_samples = None
+if( args.load_previous) :
+    flat_samples = read_samples()
+else : 
+    flat_samples = run_mcmc()
 
-
-flat_samples = sampler.get_chain(discard=discard, thin=thin, flat=True)
-print(flat_samples.shape)
-
-import corner
-
-pl.clf()
-labels = [r"$\log(\alpha_1)$",r"$\log(-\alpha_2)$", r"$V_{\rm lsr}$"]
-if MODEL == EXPONENTIAL or MODEL == GAUSSIAN : 
-    labels = [r"$\log(\rho_0/1\,M_{\odot}\,{\rm pc}^{-3})$",r"$\log(z_0/1\,{\rm pc})$", r"$V_{\rm lsr}$"]
-    flat_samples[:,0] -= math.log10(2e33/pc**3)
-    flat_samples[:,1] -= math.log10(pc)
-fig = corner.corner( flat_samples[:,0:number_parameters], labels=labels[0:number_parameters],truths=itheta[0:number_parameters])
-pl.savefig("corner.pdf")
-
-for i in range(number_parameters):
-    mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
-    q = np.diff(mcmc)
-    txt = "\mathrm{{{3}}} = {0:.3f}_{{-{1:.3f}}}^{{{2:.3f}}}"
-    print( "{0} = {1} {2} {3}".format(labels[i], mcmc[1], q[0], q[1]))
-
-tau = sampler.get_autocorr_time()
-print(tau)
+make_corner_plot( flat_samples)
